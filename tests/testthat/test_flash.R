6f7e6ac1d1204a7153c11d341e3886e3d552ec59
stephens999 <stephens999@gmail.com>
2017-09-13 15:54:41 -0500

    added simple vignette comparing with flashr

--- tests/testthat/test_flash.R
+++ tests/testthat/test_flash.R
@@ -16,26 +16,8 @@ test_that("flash update improves mean squared error in simple situation", {
 }
 )
 
-# test_that("flash r1 works with missing data", {
-#   set.seed(1)
-#   ll = rnorm(100)
-#   ff = rnorm(1000)
-#   LF = outer(ll,ff)
-#   Y = LF + rnorm(100000)
-#
-#   Y.miss = Y
-#   for(i in 1:100){ # set half of Y to be missign at random
-#     Y.miss[i,sample(1:1000,500)]=NA
-#   }
-#
-#   data = set_flash_data(Y)
-#   f= flash_r1(data)
-#
-#   data.miss = set_flash_data(Y.miss)
-#   f.miss = flash_r1(data.miss)
-#   plot(LF,flash_get_lf(f.miss))
-# }
-# )
+
+
 
 # just used to run flash.. at the moment don't have a good test!
 # test_that("flash r1 approximates solution", {
