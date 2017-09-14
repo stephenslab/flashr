8f3b0bdbc480eb3c9fb6a0312d7751f694de9362
Wei Wang <wei.wang0610099@gmail.com>
2017-09-14 00:11:49 -0500

    add lower bound

--- R/updates.R
+++ R/updates.R
@@ -14,9 +14,13 @@ flash_update_single_loading = function(data,f,k){
     Rk = get_Rk(data,f,k) #residuals excluding factor k
     x = ((Rk*tau) %*% f$EF[,k]) * s^2
     a = ashr::ash(as.vector(x),as.vector(s),outputlevel=5,mixcompdist="normal",method="shrink")
+    mat_post = list(comp_postprob=a$flash_data$comp_postprob,
+                    comp_postmean=a$flash_data$comp_postmean,
+                    comp_postmean2=a$flash_data$comp_postmean2)
     f$EL[,k] = a$flash_data$postmean
     f$EL2[,k] = a$flash_data$postmean2
     f$gl[[k]] = a$flash_data$fitted_g
+    f$matl[[k]] = mat_post
   }
   return(f)
 }
@@ -37,9 +41,13 @@ flash_update_single_factor = function(data,f,k){
     Rk = get_Rk(data,f,k) #residuals excluding factor k
     x = (t(Rk*tau) %*% f$EL[,k]) * s^2
     a = ashr::ash(as.vector(x),as.vector(s),outputlevel=5,mixcompdist="normal",method="shrink")
+    mat_post = list(comp_postprob=a$flash_data$comp_postprob,
+                    comp_postmean=a$flash_data$comp_postmean,
+                    comp_postmean2=a$flash_data$comp_postmean2)
     f$EF[,k] = a$flash_data$postmean
     f$EF2[,k] = a$flash_data$postmean2
     f$gf[[k]] = a$flash_data$fitted_g
+    f$matf[[k]] = mat_post
   }
   return(f)
 }
@@ -59,15 +67,22 @@ flash_update_single_fl = function(data,f,k){
 #' @param f a flash object
 #' @param k the index of the factor/loading to optmize
 #' @return an updated flash object
-flash_optimize_single_fl = function(data,f,k,tol=1e-2){
-  c = get_conv_criteria(f)
+flash_optimize_single_fl = function(data,f,k,tol=1e-3){
+  # c = get_conv_criteria(f)
+  f = flash_update_single_fl(data,f,k)
+  obj_val = flash_get_LB(data,f)
+  obj_track = c(obj_val)
   diff = 1
   while(diff > tol){
     f = flash_update_single_fl(data,f,k)
-    cnew = get_conv_criteria(f)
-    diff = mean((cnew-c)^2)
-    c = cnew
+    #cnew = get_conv_criteria(f)
+    obj_val_new = flash_get_LB(data,f)
+    #diff = mean((cnew-c)^2)
+    diff =abs( obj_val_new - obj_val)
+    obj_val = obj_val_new
+    obj_track = c(obj_track, obj_val)
   }
+  f$obj_track = obj_track
   return(f)
 }
 
