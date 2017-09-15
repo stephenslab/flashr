5cd45c89cac5959be12d0f351b8b64d5cda8c892
stephens999 <stephens999@gmail.com>
2017-09-14 20:05:06 -0500

    add saving of ash parameters; add a test; fix some bugs where ash parameters were not being passed

---
+++ tests/testthat/test_ash_param.R
@@ -0,0 +1,13 @@
+test_that("passing in and saving ash parameters works", {
+  set.seed(1)
+  l = rnorm(5)
+  f = rnorm(20)
+  LF = outer(l,f)
+  Y = LF + rnorm(5*20)
+  f = flash_r1(Y)
+  expect_equal(f$ash_param[[1]],flash_default_ash_param())
+  f = flash_r1(Y,ash_param=list(method="fdr"))
+  expect_equal(f$ash_param[[1]],
+               modifyList(flash_default_ash_param(),list(method="fdr")))
+}
+)
