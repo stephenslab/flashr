07794ca4c0486167070e05c18a9db718207b4999
stephens999 <stephens999@gmail.com>
2017-09-16 16:55:20 -0500

    add softimpute

--- R/flash_data.R
+++ R/flash_data.R
@@ -9,7 +9,8 @@
 #' ii) data$Y * data$missing is 0 if the original data were missing
 #' @return a flash data object
 #' @export
-set_flash_data = function(Y){
+set_flash_data = function(Y, init=c("softimpute","mean")){
+  init = match.arg(init)
   data = list(Y=NULL, anyNA=FALSE, missing = FALSE) # initialize data
 
   if(anyNA(Y)){ # deal with missing data: set flags and impute
@@ -20,7 +21,11 @@ set_flash_data = function(Y){
       stop("data must not have all missing columns")}
 
     data$anyNA=TRUE
-    Y[data$missing] = mean(Y,na.rm=TRUE)
+    if(init=="mean"){Y[data$missing] = mean(Y,na.rm=TRUE)}
+    if(init=="softimpute"){
+      m= softImpute::softImpute(Y, rank.max = 1,type = "als",lambda = 30)
+      simple_impute = outer(as.vector(m$u),as.vector(m$d * m$v))
+      Y[data$missing] = simple_impute[data$missing] }
   }
   data$Y=Y
   return(data)
