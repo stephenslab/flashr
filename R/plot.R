e4d6ece6478fc021a08569fbcc689dcdbcc5b646
stephens999 <stephens999@gmail.com>
2018-01-05 15:05:48 -0600

    remove ggplot2 from NAMESPACE

--- R/plot.R
+++ R/plot.R
@@ -7,7 +7,6 @@
 #' @param data the flash data object
 #' @param f is a flash fit object
 #' @details plots each factor and loading as a barplot.
-#' @importFrom ggplot2 ggplot
 #' @export
 flash_plot_factors = function(data,f,k = 1,loading_label = FALSE,factor_label = FALSE){
   library(ggplot2)
@@ -48,17 +47,14 @@ flash_plot_factors = function(data,f,k = 1,loading_label = FALSE,factor_label = 
 #' @param y_lab is the name of the Y axis
 #' @details plot_f is to plot the factor as barplot.
 #'
-#' @importFrom ggplot2 ggplot
-#'
 plot_one_factor = function(f,pve,k,f_labels = NA,y_lab = "factor values"){
-  library(ggplot2)
   P=length(f)
   if(any(is.na(f_labels))){
     f_dat <- data.frame(variable = 1:P, Factor = f,
                         sign.f = factor(sign(f)),
                         hjust = factor(sign(f)))
 
-    plot_f = ggplot(f_dat,aes(x = variable,y = Factor,fill = sign.f )) +
+    plot_f = ggplot2::ggplot(f_dat,aes(x = variable,y = Factor,fill = sign.f )) +
       geom_bar(stat = "identity",width = 0.5) +
       scale_fill_manual(values = c("blue","red")) +
       theme_minimal() +
@@ -74,7 +70,7 @@ plot_one_factor = function(f,pve,k,f_labels = NA,y_lab = "factor values"){
     upper_f = max(f,0) + 0.15 * range_f
     lower_f = min(f,0) - 0.15 * range_f
 
-    plot_f = ggplot(f_dat,aes(x = variable,y = Factor,label = variablenames,fill = sign.f)) +
+    plot_f = ggplot2::ggplot(f_dat,aes(x = variable,y = Factor,label = variablenames,fill = sign.f)) +
       geom_bar(stat = "identity",width = 0.5) +
       geom_text(size = 2.75,angle = 90,hjust = as.character(f_dat$hjust),
                 nudge_y = sign(f_dat$Factor) * 0.1 * mean(abs(f_dat$Factor)) ) +
@@ -89,12 +85,11 @@ plot_one_factor = function(f,pve,k,f_labels = NA,y_lab = "factor values"){
 #' plot a scree plot of proportion of variance explained by each factor
 #' @param f the flash fit object
 #' @return a ggplot plot object
-#' @importFrom ggplot2 ggplot
 #' @export
 flash_plot_pve = function(f,main="Scree plot of PVE for each factor"){
   pve = flash_get_pve(f)
   pve_dat = data.frame(factor_index = seq(1,flash_get_k(f)),PVE = pve)
-  p <- ggplot(pve_dat, aes(factor_index, PVE)) + geom_point(size = 4)  +
+  p <- ggplot2::ggplot(pve_dat, aes(factor_index, PVE)) + geom_point(size = 4)  +
     geom_line(linetype = "dotdash") + labs(title = main)
   return(p)
 }
