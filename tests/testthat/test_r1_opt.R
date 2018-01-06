a7e7a4e9d7bf8ce335a91d940d7b633df697afb2
stephens999 <stephens999@gmail.com>
2018-01-06 07:19:07 -0600

    add test for r1_opt

---
+++ tests/testthat/test_r1_opt.R
@@ -0,0 +1,18 @@
+test_that("results from r1_opt match old results", {
+  set.seed(1)
+  n=100
+  p=1000
+  ll = rnorm(n)
+  ff = rnorm(p)
+  LF = outer(ll,ff)
+  Y = LF + rnorm(n*p)
+  Y.miss = Y
+  for(i in 1:n){ # set half of Y to be missign at random
+  Y.miss[i,sample(1:p,p/2)]=NA
+  }
+  data = flash_set_data(Y)
+  f= flash_r1(data,verbose=FALSE)
+  f.old= flash_r1_old(data,verbose=FALSE)
+  expect_true(all.equal(f,f.old))
+})
+
