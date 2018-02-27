00fe7bb77fd0296be7d549bb98ab4a8862bddf69
stephens999 <stephens999@gmail.com>
2018-02-27 10:19:46 -0600

    update examples

--- R/flash.R
+++ R/flash.R
@@ -1,20 +1,20 @@
 #' @title Fit Empirical Bayes Matrix Factorization
-#' 
+#'
 #' @description This is the main interface for fitting EBMF models
 #'   based on algorithms from Wang and Stephens.  The default behaviour
 #'   is simply to run the greedy algorithm and return the result.  To
 #'   follow it by backfitting set \code{backfit = TRUE}.
-#' 
+#'
 #' @param data An n by p matrix or a flash data object created using
 #'   \code{flash_set_data}.
 #'
 #' @param Kmax Description of input argument goes here.
-#' 
+#'
 #' @param f_init if supplied, a flash object to which a single new
 #'   factor is to be added.
-#' 
+#'
 #' @param var_type type of variance structure to assume for residuals.
-#' 
+#'
 #' @param init_fn function to be used to initialize the factor. This
 #'   function should take parameters (Y,K) where Y is an n by p matrix
 #'   of data (or a flash data object) and K is a number of factors.  It
@@ -23,40 +23,40 @@
 #'   example.  (If the input data includes missing values then this
 #'   function must be able to deal with missing values in its input
 #'   matrix.)
-#' 
+#'
 #' @param tol Specify how much objective can change in a single
 #'   iteration to be considered not converged.
-#' 
+#'
 #' @param ebnm_fn function to solve the Empirical Bayes Normal Means problem
-#' 
+#'
 #' @param ebnm_param named list containing parameters to be passed to
 #'   ebnm_fn when optimizing; defaults set by flash_default_ebnm_param()
-#' 
+#'
 #' @param verbose if TRUE various output progress updates will be printed
-#' 
+#'
 #' @param nullcheck flag whether to check, after running hill-climbing
 #'   updates, whether the achieved optimum is better than setting factor
 #'   to 0. If this check is performed and fails then the factor will be
 #'   set to 0 in the returned fit.
-#' 
+#'
 #' @param seed a random number seed to use before running method - for
 #'   reproducibility. Set to NULL if you don't want seed set.  (The seed
 #'   can affect initialization when there are missing data; otherwise
 #'   the algorithm is deterministic)
-#' 
+#'
 #' @param greedy a TRUE/FALSE flag to say whether to start by running
 #'   the greedy algorithm (if FALSE then f_init must be supplied)
-#' 
+#'
 #' @param backfit a TRUE/FALSE flag to say whether to run backfitting.
-#' 
+#'
 #' @seealso flash_add_greedy, flash_backfit
-#' 
+#'
 #' @return A fitted flash object. Use \code{flash_get_ldf} to access
 #'   standardized loadings and factors; use \code{flash_get_lf} to
 #'   access fitted LF'.
-#' 
+#'
 #' @examples
-#' 
+#'
 #' set.seed(1) # for reproducibility
 #' ftrue = matrix(rnorm(200),ncol=2)
 #' ltrue = matrix(rnorm(40),ncol=2)
@@ -72,29 +72,29 @@
 #'
 #' # Plot true l against estimated l; with this seed it turns out the
 #' # 2nd loading/factor corresponds to the first column of ltrue.
-#' plot(ltrue[,1],ldf$l[,2]) 
+#' plot(ltrue[,1],ldf$l[,2])
 #'
 #' # Plot true f against estimated f (note estimate is normalized).
 #' plot(ftrue[,1],ldf$f[,2])
 #'
 #' # Plot true lf' against estimated lf'; the scale of the estimate
 #' # matches the data.
-#' plot(ltrue %*% t(ftrue), flash_get_lf(f)) 
+#' plot(ltrue %*% t(ftrue), flash_get_lf(f))
 #'
 #' # Example to use the more flexible ebnm function in ashr.
 #' f2 = flash(Y,ebnm_fn = ebnm_ash)
-#' 
+#'
 #' # Example to show how to pass parameters to ashr (may be most
 #' # useful for research use).
 #' f3 = flash(Y,ebnm_fn = ebnm_ash,
 #'            ebnm_param = list(mixcompdist = "normal",method="fdr"))
-#' 
+#'
 #' # Example to show how to use a different initialization function.
 #' library(softImpute)
 #' f4 = flash(Y,init_fn = function(x,K=1){softImpute(x,K,lambda=10)})
-#' 
+#'
 #' @export
-#' 
+#'
 flash = function(data,
                  Kmax = 100,
                  f_init = NULL,
@@ -144,7 +144,7 @@ flash = function(data,
 }
 
 #' @title Fit Empirical Bayes Matrix Factorization (greedy algorithm)
-#' 
+#'
 #' @description This implements the greedy algorithm from Wang and
 #'   Stephens. It can be used to adds factors to an existing fit, or
 #'   start from scratch.  It adds factors iteratively, at each stage
@@ -153,13 +153,13 @@ flash = function(data,
 #'   stops when an added factor contributes nothing, or Kmax is reached.
 #'   Each new factor is intialized by applying the function `init_fn` to
 #'   the residuals after removing previously-fitted factors.
-#' 
+#'
 #' @inheritParams flash_r1
 #'
 #' @param Kmax Description of input argument goes here.
-#' 
+#'
 #' @return A fitted flash object.
-#' 
+#'
 #' @examples
 #' l = rnorm(100)
 #' f = rnorm(10)
@@ -168,7 +168,7 @@ flash = function(data,
 #'
 #' # Gives the weights for each factor (analogue of singular values).
 #' flash_get_ldf(f)$d
-#' 
+#'
 #' # Example to show how to use a different initialization function.
 #' library(softImpute)
 #' f2 = flash_add_greedy(Y,10,init_fn = function(x,K=1){
@@ -176,7 +176,7 @@ flash = function(data,
 #' })
 #'
 #' @export
-#' 
+#'
 flash_add_greedy = function(data,
                             Kmax = 1,
                             f_init = NULL,
@@ -209,7 +209,7 @@ flash_add_greedy = function(data,
                  verbose,
                  nullcheck,
                  seed = NULL)
-    
+
     # Test whether the factor/loading combination is effectively zero.
     if (is_tiny_fl(f, flash_get_k(f)))
       break
@@ -220,34 +220,34 @@ flash_add_greedy = function(data,
 
 
 #' @title Refines a fit of the flash model to data by "backfitting".
-#' 
+#'
 #' @description Iterates through the factors of a flash object,
 #'   updating each until convergence.
-#' 
+#'
 #' @param f_init A fitted flash object to be refined.
-#' 
+#'
 #' @param kset The indices of factors to be optimized (NULL indicates
 #'   all factors).
-#' 
+#'
 #' @param maxiter A maximum number of iterations to perform (in the
 #'   inner loop). To perform just one iteration we suggest setting
 #'   \code{maxiter = 1} and \code{nullcheck = FALSE}.
-#' 
+#'
 #' @inheritParams flash_r1
-#' 
+#'
 #' @return A fitted flash object.
-#' 
+#'
 #' @examples
-#' 
-#' Y = matrix(rnorm(100),nrow=5,ncol=20)
+#'
+#' l = rnorm(100) #simulate some rank 1 data
+#' f = rnorm(10)
+#' Y = outer(l,f) + matrix(rnorm(1000),nrow=100)
 #' fg = flash_add_greedy(Y,10)
 #' fb = flash_backfit(Y,fg) # refines fit from greedy by backfitting
-#' fsi = flash_init_fn(flash_set_data(Y),"udv_si",4)
-#' fb2 = flash_backfit(Y,fsi)
-#' flash_get_ldf(fb2)$d
-#' 
+#' flash_get_ldf(fb)$d
+#'
 #' @export
-#' 
+#'
 flash_backfit = function(data,
                          f_init,
                          kset = NULL,
@@ -284,7 +284,7 @@ flash_backfit = function(data,
   iteration = 2
 
   while((diff > tol) & (iteration <= maxiter)) {
-      
+
     # There are two steps; first backfit, then null check
     # (if nullcheck removes some factors then the whole process is repeated)
     while((diff > tol) & (iteration <= maxiter)){
@@ -317,20 +317,20 @@ flash_backfit = function(data,
 }
 
 #' @title Fits a rank 1 Empirical Bayes Matrix Factorization model.
-#' 
+#'
 #' @return A fitted flash object. Use \code{flash_get_ldf} to access
 #'   standardized loadings and factors; use \code{flash_get_lf} to
 #'   access fitted LF'.
-#' 
+#'
 #' @inheritParams flash
-#' 
+#'
 #' @examples
-#' 
+#'
 #' ftrue = rnorm(100)
 #' ltrue = rnorm(20)
 #'
 #' # Set up a simulated matrix with rank 1 plus noise structure.
-#' Y = ltrue %*% t(ftrue)+rnorm(2000) 
+#' Y = ltrue %*% t(ftrue)+rnorm(2000)
 #' f = flash_r1(Y)
 #' ldf = flash_get_ldf(f)
 #'
@@ -346,7 +346,7 @@ flash_backfit = function(data,
 #'
 #' # Example to use the more flexible ebnm function in ashr.
 #' f2 = flash_r1(Y,ebnm_fn = ebnm_ash)
-#' 
+#'
 #' # Example to show how to pass parameters to ashr.
 #' f3 = flash_r1(Y,ebnm_fn = ebnm_ash,
 #'               ebnm_param = list(mixcompdist = "normal",method="fdr"))
