344b6a004332ae73525d7a1e7b3599b9f1704962
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-07-09 15:56:23 -0500

    handle data argument in flash_get_objective and update docs

--- R/F_objective.R
+++ R/F_objective.R
@@ -1,12 +1,16 @@
-#' @title Get objective function from flash data and flash fit objects.
-#' 
-#' @param data A flash data object.
-#' 
+#' @title Get value of objective function
+#'
+#' @description Get value of objective function from data and flash fit
+#'   object.
+#'
+#' @inheritParams flash
+#'
 #' @param f A flash fit object.
-#' 
+#'
 #' @export
-#' 
+#'
 flash_get_objective = function(data, f) {
+  data = handle_data(data)
   return(sum(unlist(f$KL_l)) + sum(unlist(f$KL_f)) + e_loglik(data, f))
 }
 
@@ -25,13 +29,13 @@ e_loglik = function(data, f) {
 #   The expectation is taken over the posterior on theta.
 #
 # @param x Observations in normal means.
-# 
+#
 # @param s Standard errors of x.
-# 
+#
 # @param Em Posterior mean of theta.
-# 
+#
 # @param Em2 Posterior second moment of theta.
-# 
+#
 NM_posterior_e_loglik = function(x, s, Et, Et2) {
   return(-0.5*sum(log(2*pi*s^2) + (1/s^2)*(Et2 - 2*x*Et + x^2)))
 }
