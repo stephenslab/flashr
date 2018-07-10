174b0dc9b5dfc08740885735dbafc6d1f58cf4dd
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-07-10 15:08:09 -0500

    fix build errors

--- tests/testthat/test_add.R
+++ tests/testthat/test_add.R
@@ -22,7 +22,7 @@ test_that("various additions work", {
   f2 = flash_init_fn(data,"udv_svd",2)
   f3 = flash_add_factors_from_data(data,1,f1,"udv_svd")
   expect_s3_class(f3, "flash")
-  expect_equal(flash_get_lf(f3),flash_get_lf(f2))
+  expect_equal(flash_get_fitted_values(f3),flash_get_fitted_values(f2))
 
   f2 = flash_init_null()
   f3 = flash_combine(f2,f1)
