0b4a622c3fb30e9c89f1c8ad04e03cbd74cb4116
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-07-10 20:02:50 -0500

    minor cosmetic changes

--- R/flash_utils.R
+++ R/flash_utils.R
@@ -69,27 +69,27 @@ flash_zero_out_factor = function(f, k) {
 #   original flash fit object interchanged.
 #
 flash_transpose = function(f) {
-    if (is.null(f)) {
-        return(NULL)
-    }
+  if (is.null(f)) {
+    return(NULL)
+  }
 
-    tmp = names(f)
-    tmp[c(which(tmp == "EL"), which(tmp == "EF"))] = c("EF", "EL")
-    tmp[c(which(tmp == "EL2"), which(tmp == "EF2"))] = c("EF2", "EL2")
-    tmp[c(which(tmp == "fixl"), which(tmp == "fixf"))] = c("fixf", "fixl")
-    tmp[c(which(tmp == "gl"), which(tmp == "gf"))] = c("gf", "gl")
-    tmp[c(which(tmp == "KL_l"), which(tmp == "KL_f"))] = c("KL_f", "KL_l")
-    tmp[c(which(tmp == "ebnm_fn_l"),
-          which(tmp == "ebnm_fn_f"))] = c("ebnm_fn_f", "ebnm_fn_l")
-    tmp[c(which(tmp == "ebnm_param_l"),
-          which(tmp == "ebnm_param_f"))] = c("ebnm_param_f", "ebnm_param_l")
-    names(f) = tmp
+  tmp = names(f)
+  tmp[c(which(tmp == "EL"), which(tmp == "EF"))] = c("EF", "EL")
+  tmp[c(which(tmp == "EL2"), which(tmp == "EF2"))] = c("EF2", "EL2")
+  tmp[c(which(tmp == "fixl"), which(tmp == "fixf"))] = c("fixf", "fixl")
+  tmp[c(which(tmp == "gl"), which(tmp == "gf"))] = c("gf", "gl")
+  tmp[c(which(tmp == "KL_l"), which(tmp == "KL_f"))] = c("KL_f", "KL_l")
+  tmp[c(which(tmp == "ebnm_fn_l"),
+        which(tmp == "ebnm_fn_f"))] = c("ebnm_fn_f", "ebnm_fn_l")
+  tmp[c(which(tmp == "ebnm_param_l"),
+        which(tmp == "ebnm_param_f"))] = c("ebnm_param_f", "ebnm_param_l")
+  names(f) = tmp
 
-    if (is.matrix(f$tau)) {
-        f$tau = t(f$tau)
-    }
+  if (is.matrix(f$tau)) {
+    f$tau = t(f$tau)
+  }
 
-    return(f)
+  return(f)
 }
 
 
@@ -101,20 +101,20 @@ flash_transpose = function(f) {
 #   flash data object transposed.
 #
 flash_transpose_data = function(data) {
-    if (is.matrix(data$Yorig)) {
-        data$Yorig = t(data$Yorig)
-    }
-    if (is.matrix(data$missing)) {
-        data$missing = t(data$missing)
-    }
-    if (is.matrix(data$Y)) {
-        data$Y = t(data$Y)
-    }
-    if (is.matrix(data$S)) {
-        data$S = t(data$S)
-    }
+  if (is.matrix(data$Yorig)) {
+    data$Yorig = t(data$Yorig)
+  }
+  if (is.matrix(data$missing)) {
+    data$missing = t(data$missing)
+  }
+  if (is.matrix(data$Y)) {
+    data$Y = t(data$Y)
+  }
+  if (is.matrix(data$S)) {
+    data$S = t(data$S)
+  }
 
-    return(data)
+  return(data)
 }
 
 
@@ -128,23 +128,23 @@ flash_transpose_data = function(data) {
 #   and f2. The precision (tau) of the combined fit is inherited from f2.
 #
 flash_combine = function(f1, f2) {
-    f = list(EL = cbind(f1$EL, f2$EL),
-             EF = cbind(f1$EF, f2$EF),
-             EL2 = cbind(f1$EL2, f2$EL2),
-             EF2 = cbind(f1$EF2, f2$EF2),
-             fixl = cbind(f1$fixl, f2$fixl),
-             fixf = cbind(f1$fixf, f2$fixf),
-             gl = c(f1$gl, f2$gl),
-             gf = c(f1$gf, f2$gf),
-             ebnm_fn_l = c(f1$ebnm_fn_l, f2$ebnm_fn_l),
-             ebnm_fn_f = c(f1$ebnm_fn_f, f2$ebnm_fn_f),
-             ebnm_param_l = c(f1$ebnm_param_l, f2$ebnm_param_l),
-             ebnm_param_f = c(f1$ebnm_param_f, f2$ebnm_param_f),
-             KL_l = c(f1$KL_l, f2$KL_l),
-             KL_f = c(f1$KL_f, f2$KL_f),
-             tau = f2$tau)
-    class(f) = "flash"
-    return(f)
+  f = list(EL = cbind(f1$EL, f2$EL),
+           EF = cbind(f1$EF, f2$EF),
+           EL2 = cbind(f1$EL2, f2$EL2),
+           EF2 = cbind(f1$EF2, f2$EF2),
+           fixl = cbind(f1$fixl, f2$fixl),
+           fixf = cbind(f1$fixf, f2$fixf),
+           gl = c(f1$gl, f2$gl),
+           gf = c(f1$gf, f2$gf),
+           ebnm_fn_l = c(f1$ebnm_fn_l, f2$ebnm_fn_l),
+           ebnm_fn_f = c(f1$ebnm_fn_f, f2$ebnm_fn_f),
+           ebnm_param_l = c(f1$ebnm_param_l, f2$ebnm_param_l),
+           ebnm_param_f = c(f1$ebnm_param_f, f2$ebnm_param_f),
+           KL_l = c(f1$KL_l, f2$KL_l),
+           KL_f = c(f1$KL_f, f2$KL_f),
+           tau = f2$tau)
+  class(f) = "flash"
+  return(f)
 }
 
 
@@ -157,14 +157,14 @@ flash_combine = function(f1, f2) {
 # @return A subsetted flash fit object.
 #
 flash_subset_l = function(f, subset) {
-    subf = f
-    subf$EL = subf$EL[subset, , drop = F]
-    subf$EL2 = subf$EL2[subset, , drop = F]
-    subf$fixl = subf$fixl[subset, , drop = F]
-    subf$tau = subf$tau[subset, , drop = F]
-    subf$KL_l = NULL
-    subf$KL_f = NULL
-    return(subf)
+  subf = f
+  subf$EL = subf$EL[subset, , drop = F]
+  subf$EL2 = subf$EL2[subset, , drop = F]
+  subf$fixl = subf$fixl[subset, , drop = F]
+  subf$tau = subf$tau[subset, , drop = F]
+  subf$KL_l = NULL
+  subf$KL_f = NULL
+  return(subf)
 }
 
 
@@ -177,14 +177,14 @@ flash_subset_l = function(f, subset) {
 # @return A subsetted flash fit object.
 #
 flash_subset_f = function(f, subset) {
-    subf = f
-    subf$EF = subf$EF[subset, , drop = F]
-    subf$EF2 = subf$EF2[subset, , drop = F]
-    subf$fixf = subf$fixf[subset, , drop = F]
-    subf$tau = subf$tau[, subset, drop = F]
-    subf$KL_l = NULL
-    subf$KL_f = NULL
-    return(subf)
+  subf = f
+  subf$EF = subf$EF[subset, , drop = F]
+  subf$EF2 = subf$EF2[subset, , drop = F]
+  subf$fixf = subf$fixf[subset, , drop = F]
+  subf$tau = subf$tau[, subset, drop = F]
+  subf$KL_l = NULL
+  subf$KL_f = NULL
+  return(subf)
 }
 
 
@@ -199,19 +199,19 @@ flash_subset_f = function(f, subset) {
 # @return A subsetted flash data object.
 #
 flash_subset_data = function(data, row_subset = NULL, col_subset = NULL) {
-    if (is.null(row_subset)) {
-        row_subset = 1:nrow(data$Y)
-    }
-    if (is.null(col_subset)) {
-        col_subset = 1:ncol(data$Y)
-    }
+  if (is.null(row_subset)) {
+    row_subset = 1:nrow(data$Y)
+  }
+  if (is.null(col_subset)) {
+    col_subset = 1:ncol(data$Y)
+  }
 
-    subdata = data
-    subdata$Yorig = subdata$Yorig[row_subset, col_subset, drop = F]
-    subdata$anyNA = anyNA(subdata$Yorig)
-    subdata$missing = subdata$missing[row_subset, col_subset, drop = F]
-    subdata$Y = subdata$Y[row_subset, col_subset, drop = F]
-    class(subdata) = "flash_data"
+  subdata = data
+  subdata$Yorig = subdata$Yorig[row_subset, col_subset, drop = F]
+  subdata$anyNA = anyNA(subdata$Yorig)
+  subdata$missing = subdata$missing[row_subset, col_subset, drop = F]
+  subdata$Y = subdata$Y[row_subset, col_subset, drop = F]
+  class(subdata) = "flash_data"
 
-    return(subdata)
+  return(subdata)
 }
--- R/init.R
+++ R/init.R
@@ -20,28 +20,29 @@
 #' @importFrom assertthat assert_that
 #'
 flash_init_lf = function(LL, FF, fixl = NULL, fixf = NULL) {
-    assert_that(ncol(LL) == ncol(FF))
-    if (is.null(fixl)) {
-        fixl = matrix(FALSE, ncol = ncol(LL), nrow = nrow(LL))
-    }
-    if (is.null(fixf)) {
-        fixf = matrix(FALSE, ncol = ncol(FF), nrow = nrow(FF))
-    }
+  assert_that(ncol(LL) == ncol(FF))
+  if (is.null(fixl)) {
+    fixl = matrix(FALSE, ncol = ncol(LL), nrow = nrow(LL))
+  }
+  if (is.null(fixf)) {
+    fixf = matrix(FALSE, ncol = ncol(FF), nrow = nrow(FF))
+  }
 
-    f = list(EL = LL, EF = FF, EL2 = LL^2, EF2 = FF^2, fixl = fixl,
-             fixf = fixf)
+  f = list(EL = LL, EF = FF, EL2 = LL^2, EF2 = FF^2,
+           fixl = fixl, fixf = fixf)
 
-    f$gl = list()
-    f$gf = list()
-    f$ebnm_fn_l = list()
-    f$ebnm_fn_f = list()
-    f$ebnm_param_l = list()
-    f$ebnm_param_f = list()
-    f$KL_l = as.list(rep(0, flash_get_k(f)))
-    f$KL_f = as.list(rep(0, flash_get_k(f))) # KL divergences for each l and f.
-    f = c(f, list(tau = NULL))
-    class(f) = "flash"
-    return(f)
+  f$gl = list()
+  f$gf = list()
+  f$ebnm_fn_l = list()
+  f$ebnm_fn_f = list()
+  f$ebnm_param_l = list()
+  f$ebnm_param_f = list()
+  f$KL_l = as.list(rep(0, flash_get_k(f)))
+  f$KL_f = as.list(rep(0, flash_get_k(f)))
+  f = c(f, list(tau = NULL))
+  class(f) = "flash"
+
+  return(f)
 }
 
 # @title Initialize an empty flash fit object.
@@ -49,11 +50,16 @@ flash_init_lf = function(LL, FF, fixl = NULL, fixf = NULL) {
 # @return An empty flash fit object.
 #
 flash_init_null = function() {
-    f = list(EL = NULL, EF = NULL, fixl = NULL, fixf = NULL, EL2 = NULL, EF2 = NULL, gl = NULL, gf = NULL, ebnm_param_l = NULL,
-        ebnm_param_f = NULL, KL_l = NULL, KL_f = NULL, tau = NULL)
-    class(f) = "flash"
-    return(f)
+  f = list(EL = NULL, EF = NULL, EL2 = NULL, EF2 = NULL,
+           fixl = NULL, fixf = NULL, gl = NULL, gf = NULL,
+           ebnm_fn_l = NULL, ebnm_fn_f = NULL,
+           ebnm_param_l = NULL, ebnm_param_f = NULL,
+           KL_l = NULL, KL_f = NULL, tau = NULL)
+  class(f) = "flash"
+
+  return(f)
 }
+
 
 # @title Initialize a flash fit object by applying a function to data.
 #
@@ -73,8 +79,10 @@ flash_init_fn = function(data, init_fn, K=1) {
   f_names = colnames(get_Yorig(data))
   rownames(f$EL) = rownames(f$EL2) = rownames(f$fixl) = l_names
   rownames(f$EF) = rownames(f$EF2) = rownames(f$fixf) = f_names
+
   return(f)
 }
+
 
 # @title Initialize a flash fit object from a list with elements (u,d,v).
 #
@@ -85,24 +93,25 @@ flash_init_fn = function(data, init_fn, K=1) {
 # @return A flash fit object ready for optimization.
 #
 flash_init_udv = function(s, K = 1) {
-    s$u = as.matrix(s$u)
-    s$v = as.matrix(s$v)
+  s$u = as.matrix(s$u)
+  s$v = as.matrix(s$v)
 
-    # Deals with case these are vectors (K = 1).
-    if (ncol(s$u) > K)
-        {
-            s$u = s$u[, 1:K, drop = FALSE]
-        }
-    if (ncol(s$v) > K) {
-        s$v = s$v[, 1:K, drop = FALSE]
-    }
-    if (length(s$d) > K) {
-        s$d = s$d[1:K]
-    }
+  # Deals with case these are vectors (K = 1).
+  if (ncol(s$u) > K)
+  {
+    s$u = s$u[, 1:K, drop = FALSE]
+  }
+  if (ncol(s$v) > K) {
+    s$v = s$v[, 1:K, drop = FALSE]
+  }
+  if (length(s$d) > K) {
+    s$d = s$d[1:K]
+  }
 
-    f = flash_init_lf(t(s$d * t(s$u)), s$v)
-    return(f)
+  f = flash_init_lf(t(s$d * t(s$u)), s$v)
+  return(f)
 }
+
 
 # @title udv_si
 #
@@ -123,16 +132,13 @@ udv_si = function(Y, K = 1) {
   return(res)
 }
 
+
 # @title udv_si_svd
 #
 # @description provides a simple wrapper to \code{softImpute} to
 #   provide a rank 1 initialization. Uses \code{type = "svd"} option.
 #
-# @param Y an n by p matrix
-#
-# @param K number of factors to use
-#
-# @return A list with components (u,d,v)
+# @inherit udv_si
 #
 #' @importFrom softImpute softImpute
 #'
@@ -141,27 +147,23 @@ udv_si_svd = function(Y, K = 1) {
   return(res)
 }
 
+
 # @title udv_svd
 #
 # @description Provides a simple wrapper to svd.
 #
-# @param Y An n by p matrix.
-#
-# @param K Number of factors to use.
-#
-# @return A list with components (u,d,v).
+# @inherit udv_si
 #
 udv_svd = function (Y, K = 1) {
   svd(Y, K, K)
 }
 
+
 # @title udv_random
 #
 # @description Provides a random initialization of factors.
 #
-# @param Y An n by p matrix.
-#
-# @param K Number of factors.
+# @inherit udv_si
 #
 # @return A list with components (u,d,v), with elements of u and v
 #   i.i.d. N(0,1).
