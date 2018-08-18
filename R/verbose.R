71ee919dd4b0fefdc74ec65d70e8f228d5a4d6da
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-08-18 18:13:31 -0400

    fix verbose sparsity to say "NA" when ebnm_fn != ebnm_pn

--- R/verbose.R
+++ R/verbose.R
@@ -29,7 +29,6 @@ stopping_criterion_string = function(stopping_rule, tol) {
 
 verbose_obj_table_header = function(verbose_output) {
   header_string = "  Iteration"
-
   if ("l" %in% verbose_output) {
     header_string = paste0(header_string,
                            sprintf("%9s", "pi0 (l)"))
@@ -38,7 +37,6 @@ verbose_obj_table_header = function(verbose_output) {
     header_string = paste0(header_string,
                            sprintf("%9s", "pi0 (f)"))
   }
-
   if ("L" %in% verbose_output) {
     header_string = paste0(header_string,
                            sprintf("%13s", "Max Chg (l)"))
@@ -47,7 +45,6 @@ verbose_obj_table_header = function(verbose_output) {
     header_string = paste0(header_string,
                            sprintf("%13s", "Max Chg (f)"))
   }
-
   if ("o" %in% verbose_output) {
     header_string = paste0(header_string,
                            sprintf("%15s", "Objective"))
@@ -56,23 +53,20 @@ verbose_obj_table_header = function(verbose_output) {
     header_string = paste0(header_string,
                            sprintf("%11s", "Obj Diff"))
   }
-
   message(header_string)
 }
 
 verbose_obj_table_entry = function(verbose_output, iter, obj, obj_diff,
                                    max_chg_l, max_chg_f, gl, gf) {
   entry_string = sprintf("%11d", iter)
-
   if ("l" %in% verbose_output) {
     l_sparsity = verbose_sparsity(gl)
-    entry_string = paste0(entry_string, sprintf("%9.3f", l_sparsity))
+    entry_string = paste0(entry_string, sprintf("%9s", l_sparsity))
   }
   if ("f" %in% verbose_output) {
     f_sparsity = verbose_sparsity(gf)
-    entry_string = paste0(entry_string, sprintf("%9.3f", f_sparsity))
+    entry_string = paste0(entry_string, sprintf("%9s", f_sparsity))
   }
-
   if ("L" %in% verbose_output) {
     entry_string = paste0(entry_string,
                           sprintf("%12.2f", 100 * max_chg_l), "%")
@@ -81,19 +75,26 @@ verbose_obj_table_entry = function(verbose_output, iter, obj, obj_diff,
     entry_string = paste0(entry_string,
                           sprintf("%12.2f", 100 * max_chg_f), "%")
   }
-
   if ("o" %in% verbose_output) {
     entry_string = paste0(entry_string,
                           sprintf("%15.2f", obj))
   }
-
   if ("d" %in% verbose_output) {
     diff_string = formatC(obj_diff, format="e", digits=2)
     entry_string = paste0(entry_string,
                           sprintf("%11s", diff_string))
   }
-
   message(entry_string)
+}
+
+# At present, only returns nonnull for ebnm_pn.
+verbose_sparsity = function(g) {
+  if (is.null(g[[1]]$pi0)) {
+    return("NA")
+  } else {
+    s = mean(sapply(g, function(k) {k$pi0}))
+    return(formatC(s, format = "f", digits = 3))
+  }
 }
 
 verbose_obj_decrease_warning = function() {
@@ -126,13 +127,4 @@ verbose_nullcheck_keep_fl = function(k, diff) {
 verbose_nullcheck_complete = function(obj) {
   message("  Nullcheck complete. Objective: ",
           round(obj, digits=2))
-}
-
-# At present, only returns nonnull for ebnm_pn.
-verbose_sparsity = function(g) {
-  if (is.null(g[[1]]$pi0)) {
-    return(NULL)
-  } else {
-    return(mean(sapply(g, function(k) {k$pi0})))
-  }
 }
