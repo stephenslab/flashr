f6ffae5e87a7829fc045e589ef2aba1cfb86f3fe
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-08-23 14:57:35 -0400

    Normalize EF and EL by mean absolute value, not max absolute value (so that tol has similar meaning regardless of scale)

--- R/stopping_rule.R
+++ R/stopping_rule.R
@@ -16,34 +16,29 @@ is_converged = function(stopping_rule, tol, obj_diff, max_chg_l, max_chg_f) {
   }
 }
 
-# Normalizes EL and EF before changes in parameter values are calculated.
+# Normalizes EL and EF before changes in parameter values are calculated. In
+#   order for `tol` to have a similar meaning regardless of the size of the
+#   data set, we normalize each loading (i.e., each column of EL) so that the
+#   mean absolute value of the elements of the loading is equal to one (and
+#   similarly for the factors).
 #
 normalize_lf = function(EL, EF) {
-  # normalize both factors and loadings to have l_1 norm equal to 1:
   if (is.matrix(EL)) {
-    lnorms = apply(abs(EL), 2, max)
+    lnorms = apply(abs(EL), 2, mean)
     EL = as.vector(sweep(EL, 2, lnorms, `/`))
-    fnorms = apply(abs(EF), 2, max)
+    fnorms = apply(abs(EF), 2, mean)
     EF = as.vector(sweep(EF, 2, fnorms, `/`))
   } else {
-    EL = EL / max(abs(EL))
-    EF = EF / max(abs(EF))
+    EL = EL / mean(abs(EL))
+    EF = EF / mean(abs(EF))
   }
-
-  # # normalize loadings to have l_2 norm equal to 1:
-  # if (is.matrix(EL)) {
-  #   norms = sqrt(colSums(EL^2))
-  #   EL = as.vector(sweep(EL, 2, norms, `/`))
-  #   EF = as.vector(sweep(EF, 2, norms, `*`))
-  # } else {
-  #   norm = sqrt(sum(EL^2))
-  #   EL = EL / norm
-  #   EF = EF * norm
-  # }
 
   return(list(EL = EL, EF = EF))
 }
 
+# Calculates the maximum change in parameter values. Since EL and EF are
+#   normalized, we use absolute changes rather than relative ones.
+#
 calc_max_chg = function(new_vals, old_vals) {
   # Absolute difference:
   chgs = abs(new_vals - old_vals)
