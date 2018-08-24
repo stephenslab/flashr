ff800fbe29cdf7c0aa473f0262252ce63440bc0f
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-08-24 14:15:04 -0400

    move add_lf to add_fixed.R and rename flash_add to add_from_data

--- R/add_fixed.R
+++ R/add_fixed.R
@@ -147,6 +147,46 @@ flash_add_fixed_factors = function(data,
 }
 
 
+# @title Add factor/loading pairs to a flash object
+#
+# @description Adds specified factor/loading pairs to a flash object.
+#
+# @inheritParams flash
+#
+# @param LL The loadings, an n by K matrix.
+#
+# @param FF The factors, a p by K matrix.
+#
+# @param fixl An n by K matrix of \code{TRUE}/\code{FALSE} values
+#   indicating which elements of \code{LL} should be considered fixed
+#   and not changed during updates. Useful for including a mean factor
+#   for example.
+#
+# @param fixf A p by K matrix of \code{TRUE}/\code{FALSE} values; same
+#   as \code{fixl} but for factors \code{FF}.
+#
+# @return A flash fit object, with additional factors initialized
+#   using \code{LL} and \code{FF}.
+#
+flash_add_lf = function(data,
+                        LL,
+                        FF,
+                        f_init = NULL,
+                        fixl = NULL,
+                        fixf = NULL) {
+  f_init = handle_f(f_init, init_null_f = TRUE)
+  data = handle_data(data, f_init)
+  LL = handle_LL(LL, expected_nrow = flash_get_n(f_init))
+  FF = handle_LL(FF, expected_nrow = flash_get_p(f_init))
+  # fixl and fixf are handled by flash_init_lf
+
+  f2 = flash_init_lf(LL, FF, fixl, fixf)
+  f = flash_combine(f_init, f2)
+
+  return(f)
+}
+
+
 # @title Partition a matrix into blocks of identical columns.
 #
 # @param X the matrix to be partitioned (note that X should not have NAs).
