3b293d3d4472c880e506262101c4631d625f7144
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-09-21 22:26:39 -0400

    re-fix test_scalar_tau

--- tests/testthat/test_scalar_tau.R
+++ tests/testthat/test_scalar_tau.R
@@ -4,7 +4,6 @@ test_that("storing tau as a scalar works", {
   f = rnorm(20)
   LF = outer(l,f)
   Y = LF + rnorm(5*20)
-  data = flash_set_data(Y)
 
   # test that objective is same whether S is matrix or scalar:
   data_scalar = flash_set_data(Y, S = 1)
@@ -18,26 +17,26 @@ test_that("storing tau as a scalar works", {
 
   # Test that calc_ebnm_args gives same results whether we pass in a
   #   scalar or matrix with var_type = "constant":
-  largs1 = calc_ebnm_l_args(data, fl1$fit, 1, 1:5, FALSE,
-                            flash_get_Rk(data, fl1$fit, 1))
+  largs1 = calc_ebnm_l_args(data_scalar, fl1$fit, 1, 1:5, FALSE,
+                            flash_get_Rk(data_scalar, fl1$fit, 1))
   expect_length(largs1$s, 1)
   fl2 = fl1
   fl2$fit$tau = matrix(fl1$fit$tau, nrow=5, ncol=20)
-  largs2 = calc_ebnm_l_args(data, fl2$fit, 1, 1:5, FALSE,
-                            flash_get_Rk(data, fl2$fit, 1))
+  largs2 = calc_ebnm_l_args(data_matrix, fl2$fit, 1, 1:5, FALSE,
+                            flash_get_Rk(data_matrix, fl2$fit, 1))
   expect_equal(largs1$x, largs2$x)
   expect_equal(largs1$s, largs2$s[1])
 
-  fargs1 = calc_ebnm_f_args(data, fl1$fit, 1, 1:20, FALSE,
-                            flash_get_Rk(data, fl1$fit, 1))
+  fargs1 = calc_ebnm_f_args(data_scalar, fl1$fit, 1, 1:20, FALSE,
+                            flash_get_Rk(data_scalar, fl1$fit, 1))
   expect_length(fargs1$s, 1)
-  fargs2 = calc_ebnm_f_args(data, fl2$fit, 1, 1:20, FALSE,
-                            flash_get_Rk(data, fl2$fit, 1))
+  fargs2 = calc_ebnm_f_args(data_matrix, fl2$fit, 1, 1:20, FALSE,
+                            flash_get_Rk(data_matrix, fl2$fit, 1))
   expect_equal(fargs1$x, fargs2$x)
   expect_equal(fargs1$s, fargs2$s[1])
 
-  expect_equal(flash_get_pve(data, fl1),
-               flash_get_pve(data, fl2))
+  expect_equal(flash_get_pve(data_scalar, fl1),
+               flash_get_pve(data.matrix, fl2))
 
   # Repeat with missing data:
   Y[sample(1:100, 10, replace=FALSE)] = NA
@@ -47,23 +46,21 @@ test_that("storing tau as a scalar works", {
   fl_matrix = flash(data_matrix, var_type="zero")
   expect_identical(fl_scalar$objective, fl_matrix$objective)
 
-  data = flash_set_data(Y)
-
-  largs1 = calc_ebnm_l_args(data, fl1$fit, 1, 1:5, FALSE,
-                            flash_get_Rk(data, fl1$fit, 1))
+  largs1 = calc_ebnm_l_args(data_scalar, fl1$fit, 1, 1:5, FALSE,
+                            flash_get_Rk(data_scalar, fl1$fit, 1))
   expect_length(largs1$s, 5) # SEs differ where there is missing data
   fl2 = fl1
   fl2$fit$tau = matrix(fl1$fit$tau, nrow=5, ncol=20)
-  largs2 = calc_ebnm_l_args(data, fl2$fit, 1, 1:5, FALSE,
-                            flash_get_Rk(data, fl2$fit, 1))
+  largs2 = calc_ebnm_l_args(data_matrix, fl2$fit, 1, 1:5, FALSE,
+                            flash_get_Rk(data_matrix, fl2$fit, 1))
   expect_equal(largs1$x, largs2$x)
   expect_equal(largs1$s, largs2$s)
 
-  fargs1 = calc_ebnm_f_args(data, fl1$fit, 1, 1:20, FALSE,
-                            flash_get_Rk(data, fl1$fit, 1))
+  fargs1 = calc_ebnm_f_args(data_scalar, fl1$fit, 1, 1:20, FALSE,
+                            flash_get_Rk(data_scalar, fl1$fit, 1))
   expect_length(fargs1$s, 20)
-  fargs2 = calc_ebnm_f_args(data, fl2$fit, 1, 1:20, FALSE,
-                            flash_get_Rk(data, fl2$fit, 1))
+  fargs2 = calc_ebnm_f_args(data_matrix, fl2$fit, 1, 1:20, FALSE,
+                            flash_get_Rk(data_matrix, fl2$fit, 1))
   expect_equal(fargs1$x, fargs2$x)
   expect_equal(fargs1$s, fargs2$s)
 })
