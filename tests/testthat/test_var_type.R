c01b9a2954154472994d552e370c31286d59116e
Jason Willwerscheid <willwerscheid@uchicago.edu>
2018-10-28 15:20:47 -0400

    rewrite var_type tests

--- tests/testthat/test_var_type.R
+++ tests/testthat/test_var_type.R
@@ -1,23 +1,31 @@
-test_that("various var_types work as expected", {
-  set.seed(1)
-  l = rep(5,5)
-  f = rnorm(20)
-  LF = outer(l,f)
-  Y = LF + 2*rnorm(5*20)
+context("var_type")
 
-  # var_type = zero
-  data <- flash_set_data(Y, S=2)
-  fl <- flash_add_greedy(data, 1, var_type="zero")$fit
-  expect_equal(fl$tau, 0.25)
+set.seed(1)
 
-  # check for error when trying to pass bad arguments to S
-  expect_error(flash_set_data(Y, S=1:5))
-  expect_error(flash_set_data(Y, matrix(1, nrow=2, ncol=2)))
+test_that("zero and constant var_types work as expected", {
+  # rank-zero data with sd = 2
+  Y = matrix(2 * rnorm(5 * 20), nrow = 5, ncol = 20)
+  fl = flash(Y, S = 2, var_type = "zero", greedy_Kmax = 1)
+  expect_equal(fl$fit$tau, 0.25)
 
-  # var_type = by_column, by_row
-  Y = LF + c(rnorm(5 * 10), 2 * rnorm(5 * 10))
-  flcol <- flash_add_greedy(Y, 1, var_type="by_column")$fit
-  expect_setequal(flcol$tau[2:nrow(Y), 1], flcol$tau[1, 1])
-  flrow <- flash_add_greedy(t(Y), 1, var_type="by_row")$fit
-  expect_equal(flcol$tau, t(flrow$tau), tolerance=0.001)
+  flconst = flash(Y, var_type = "constant", greedy_Kmax = 1)
+  expect_equal(flconst$fit$tau, 1 / sd(Y)^2, tol = 0.01)
 })
+
+test_that("by_column and by_row var_types work as expected", {
+  # rank-zero data: first column has sd = 1; second has sd = 2
+  Y = matrix(rnorm(50 * 2, sd = rep(c(1, 2), each = 50)),
+             nrow = 50, ncol = 2)
+
+  flcol = flash(Y, var_type = "by_column", greedy_Kmax = 1)
+  expect_setequal(flcol$fit$tau[, 1], flcol$fit$tau[1, 1])
+  expect_equal(flcol$fit$tau[1, 1], 1 / sd(Y[, 1])^2, tol = 0.01)
+
+  flrow = flash(t(Y), var_type="by_row", greedy_Kmax = 1)
+  expect_equal(t(flrow$fit$tau), flcol$fit$tau, tol = 0.001)
+})
+
+# TODO: move to param check tests
+# check for error when trying to pass bad arguments to S
+# expect_error(flash_set_data(Y, S=1:5))
+# expect_error(flash_set_data(Y, matrix(1, nrow=2, ncol=2)))
